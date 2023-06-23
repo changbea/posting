3e579699f6ef0fdaefc570bd1244b633d2c01427
changbea <68812384+changbea@users.noreply.github.com>
2023-06-24 02:29:14 +0900

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import Cards from './components/Cards';
 import Page from './recommendations/router/Page';
 import Resume from './recommendations/router/Resume';
 import League from './leagues/Leagues'
-import Gamersp from './components/Gamersp';
-import Gamesrspextended from './components/Gamesrspextended';
-import Gamefinding from './components/Gamefinding';
-import Gameflipping from './components/Gameflipping';
-import Gamehint from './components/Gamehint';
+// import Gamersp from './components/Gamersp';
+// import Gamesrspextended from './components/Gamesrspextended';
+// import Gamefinding from './components/Gamefinding';
+// import Gameflipping from './components/Gameflipping';
+// import Gamehint from './components/Gamehint';
 
 export default function App() {
   return (
@@ -26,11 +26,11 @@ export default function App() {
           <Route path="/recommendations" element = { <Page /> } />
           <Route path="/resume" element = { <Resume /> } />
           <Route path="/leagues" element = { <League/> } />
-          <Route path="/gamersp" element = { <Gamersp /> } />
-          <Route path="/gamesrspextended" element = { <Gamesrspextended /> } />
-          <Route path="/gamefinding" element = { <Gamefinding /> } />
+          {/* <Route path="/gamersp" element = { <Gamersp /> } />
+          <Route path="/gamesrspextended" element = { <Gamesrspextended /> } /> */}
+          {/* <Route path="/gamefinding" element = { <Gamefinding /> } />
           <Route path="/gameflipping" element = { <Gameflipping /> } />
-          <Route path="/gamehint" element = { <Gamehint /> } />
+          <Route path="/gamehint" element = { <Gamehint /> } /> */}
         </Routes>
       </BrowserRouter>
     </div>
